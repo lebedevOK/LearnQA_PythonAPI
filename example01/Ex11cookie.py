7289cd39f655a182b86ffa56d64da942a68145cd
lebedevOK <teh161@yandex.ru>
2022-07-30 23:54:33 +0300

    Ex11: cookie

--- example01/Ex11cookie.py
+++ example01/Ex11cookie.py
@@ -5,4 +5,5 @@ class TestCookie:
     def test_cookie(self):
 
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+        print(response.cookies)
         assert response.cookies.get('HomeWork') == 'hw_value', "Cookie invalid"
